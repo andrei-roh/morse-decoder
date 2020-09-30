33146caf38165e459e60e45ec8d534e9422fd5af
Andrei Roh <andrei_roh@tut.by>
2020-09-30 11:07:48 +0300

    init: start morse-decoder task
    Added task solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let decodeStr = [];
+  for (let i = 0; i < expr.length; i += 10) {
+      let substring = expr.substring(i, i + 10);
+      let decodeSymbol = replaceSymbol(substring);
+      if (decodeSymbol === " ") {
+          decodeStr.push(decodeSymbol);
+      } else {
+          decodeStr.push(MORSE_TABLE[decodeSymbol]);
+      }
+  }
+  return decodeStr.join('');
+}
+function replaceSymbol(substring) {
+  let decodeLetter = "";
+  if (substring === "**********") {
+    return " ";
+  }
+  for (let j = 0; j < substring.length; j += 2) {
+      let letter = substring.substring(j, j + 2);
+      if (letter === "10") {
+          decodeLetter += ".";
+      } else if (letter === "11") {
+          decodeLetter += "-";
+      }
+  }
+  return decodeLetter;
 }
 
 module.exports = {
